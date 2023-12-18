69e4c6f2c707c4cd7cbb3b045ad6882cc59b94a2
0123-smallbottle <smallbottle9487@gmail.com>
2023-12-18 22:54:17 +0800

    Refactor AboutPage widget and add Image and TextWidget classes

--- lib/pages/about/view.dart
+++ lib/pages/about/view.dart
@@ -11,39 +11,80 @@ class AboutPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('About'),
       ),
-      body: Center(
-        child: ListView(
-          children: [
-            const CircleAvatar(
-              radius: 128,
-              backgroundImage: AssetImage('assets/images/profile_pic.jpg'),
-            ),
-            const SizedBox(height: 16),
-            const Text(
+
+    body: const Column(
+      children: [
+        Image(),
+        TextWidget(),
+      ],
+    ),
+    );
+  }
+}
+
+class Image extends StatelessWidget {
+  const Image({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        width: 200,
+        height: 200,
+        decoration: const BoxDecoration(
+          borderRadius: BorderRadius.all(Radius.circular(200)),
+          image: DecorationImage(
+            fit: BoxFit.fitHeight,
+            image: AssetImage("assets/images/profile_pic.jpg"),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+
+class TextWidget extends StatelessWidget {
+  const TextWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: ListView(
+        shrinkWrap: true,
+        children: [
+          const SizedBox(height: 16),
+          const Center(
+            child: Text(
               'random picker',
               style: TextStyle(
                 fontSize: 64,
                 fontWeight: FontWeight.bold,
               ),
             ),
-            const SizedBox(height: 4),
-            const Text(
+          ),
+          const SizedBox(height: 4),
+          const Center(
+            child: Text(
               'made with ❤️ by smallbottle',
               style: TextStyle(
                 fontSize: 24,
                 color: Colors.grey,
               ),
             ),
-            const SizedBox(height: 4),
-            IconButton(
+          ),
+          const SizedBox(height: 4),
+          Center(
+            child: IconButton(
               onPressed: () {
-                launchUrl(Uri.parse('https://github.com/0123-smallbottle/random_picker'));
+                launchUrl(Uri.parse(
+                    'https://github.com/0123-smallbottle/random_picker'));
               },
               icon: const Icon(FontAwesomeIcons.github),
               iconSize: 64,
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
